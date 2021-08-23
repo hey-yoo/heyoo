1cb5b08d9c9efe09b6ec0708d9210b4f1f273d90
HaolinHom <haolinhom@gmail.com>
2021-08-23 17:32:33 +0800

    feat: Use registry utils to register commands in batches;

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,36 @@
 import { Command } from 'commander';
-import { text } from 'std-terminal-logger';
-import run from './commands/run';
+import { log, label, text } from 'std-terminal-logger';
+import commands from './commands';
+import registry from './utils/registry';
+import { validate, predicates } from './utils/validate';
+import { commandItem, heyConfig } from './types';
+import { RESERVED_WORD } from './constants';
 
 const program = new Command();
 
 program.name('hey').version('v1.0.0').usage(text.green('[command]'));
 // .description(packageJson.description);
 
-program
-  .command('run [command]')
-  .description(text.purple('run customize command'))
-  .action(run);
+const outsideCommandConfigs = [] as heyConfig[];
 
-program.parse(process.argv);
+registry(
+  program,
+  /**
+  Filter out external commands that do not meet the requirements
+  */
+  commands.concat(
+    // @ts-ignore
+    outsideCommandConfigs.filter((item) => {
+      const validateErr = validate(item, item.name, predicates.heyConfig);
+      if (validateErr) {
+        log(label.error, validateErr);
+        return false;
+      }
+      return !item.registry.some(
+        (reg) => RESERVED_WORD.indexOf(reg.command) > -1
+      );
+    })
+  )
+);
+
+program.parseAsync(process.argv);
