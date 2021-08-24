abb9c3f6be79f2f3adcc6c9b67b1dc679c805549
HaolinHom <haolinhom@gmail.com>
2021-08-24 17:53:52 +0800

    fix: modify entry file;

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
+import path from 'path';
 import { Command } from 'commander';
 import { log, label, text } from 'std-terminal-logger';
 import commands from './commands';
 import registry from './utils/registry';
+import requirePackageJson from './utils/requirePackageJson';
 import { validate, predicates } from './utils/validate';
-import { commandItem, heyConfig } from './types';
-import { RESERVED_WORD } from './constants';
+import { heyConfig } from './types';
+import { ART_WORD_HEY_YOO, RESERVED_WORD } from './constants';
+import { pathExtra } from 'hey-yoo-utils';
+
+const { __dirname } = pathExtra.getGlobalPath(import.meta.url);
+const packageJson = requirePackageJson(
+  path.resolve(__dirname, '../package.json')
+);
 
 const program = new Command();
 
-program.name('hey').version('v1.0.0').usage(text.green('[command]'));
-// .description(packageJson.description);
+program
+  .name('hey')
+  .version(packageJson?.version || '')
+  .usage(text.green('[command]'))
+  .description(`${text.amber(ART_WORD_HEY_YOO)}
+
+${packageJson?.description}`);
 
 const outsideCommandConfigs = [] as heyConfig[];
 
