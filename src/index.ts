af82d67a12a64ce8879b7f1cea5ab4a21a77cd2e
HaolinHom <haolinhom@gmail.com>
2021-08-27 17:59:33 +0800

    fix: correct get all plugins' registry command logic;

--- src/index.ts
+++ src/index.ts
@@ -3,47 +3,54 @@ import { Command } from 'commander';
 import { log, label, text } from 'std-terminal-logger';
 import commands from './commands';
 import registry from './utils/registry';
-import requirePackageJson from './utils/requirePackageJson';
 import { validate, predicates } from './utils/validate';
 import { pluginsConfig } from './types';
 import { ART_WORD_HEY_YOO, RESERVED_WORD } from './constants';
-import { pathExtra } from 'hey-yoo-utils';
+import { pathExtra, fsExtra } from 'hey-yoo-utils';
+import { getAllPluginsConfig } from './utils/getConfig';
 
 const { __dirname } = pathExtra.getGlobalPath(import.meta.url);
-const packageJson = requirePackageJson(
-  path.resolve(__dirname, '../package.json')
-);
 
-const program = new Command();
+async function hey() {
+  const packageJson = fsExtra.readJson(
+    path.resolve(__dirname, '../package.json')
+  );
 
-program
-  .name('hey')
-  .version(packageJson?.version || '')
-  .usage(text.green('[command]'))
-  .description(`${text.amber(ART_WORD_HEY_YOO)}
+  const program = new Command();
+
+  program
+    .name('hey')
+    .version(packageJson?.version || '')
+    .usage(text.green('[command]')).description(`${text.amber(ART_WORD_HEY_YOO)}
 
 ${packageJson?.description}`);
 
-const outsideCommandConfigs = [] as pluginsConfig[];
+  let outsideCommandConfigs = (await getAllPluginsConfig()) as pluginsConfig[];
+  outsideCommandConfigs = outsideCommandConfigs.filter((item) => {
+    const validateErr = validate(item, item.name, predicates.pluginsConfig);
+    if (validateErr) {
+      log(label.error, validateErr);
+      return false;
+    }
+    return !item.registry.some(
+      (reg) => RESERVED_WORD.indexOf(reg.command) > -1
+    );
+  });
 
-registry(
-  program,
-  /**
-  Filter out plugins commands that do not meet the requirements
-  */
-  commands.concat(
-    // @ts-ignore
-    outsideCommandConfigs.filter((item) => {
-      const validateErr = validate(item, item.name, predicates.pluginsConfig);
-      if (validateErr) {
-        log(label.error, validateErr);
-        return false;
-      }
-      return !item.registry.some(
-        (reg) => RESERVED_WORD.indexOf(reg.command) > -1
-      );
-    })
-  )
-);
+  registry(
+    program,
+    /**
+     Filter out plugins commands that do not meet the requirements
+     */
+    commands.concat(
+      // @ts-ignore
+      ...outsideCommandConfigs
+        .map((item) => item.registry)
+        .reduce((accumulate, current) => accumulate.concat(current), [])
+    )
+  );
 
-program.parseAsync(process.argv);
+  program.parseAsync(process.argv);
+}
+
+hey();
