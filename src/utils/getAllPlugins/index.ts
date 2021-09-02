c7c536bb30683b8974eb2b6f6b28194d1f4bb897
HaolinHom <haolinhom@gmail.com>
2021-09-02 10:19:43 +0800

    fix: Improve the friendliness of error prompts;

--- src/utils/getAllPlugins/index.ts
+++ src/utils/getAllPlugins/index.ts
@@ -9,12 +9,18 @@ import { getApplication } from '../application';
 import { localPluginsPath } from '../path';
 import { label, log, text } from 'std-terminal-logger';
 
+interface plugins {
+  name: string;
+  path: string;
+}
+
 export async function getAllPlugins(): Promise<command[]> {
   const appJson = getApplication();
 
-  const installed = appJson.plugins.installed.map((item) =>
-    path.resolve(localPluginsPath, item)
-  );
+  const installed: plugins[] = appJson.plugins.installed.map((item) => ({
+    name: item,
+    path: path.resolve(localPluginsPath, item),
+  }));
   const linked = appJson.plugins.linked
     .map((item) => {
       const linkPath = path.resolve(localPluginsPath, item);
@@ -24,23 +30,30 @@ export async function getAllPlugins(): Promise<command[]> {
         const pkg = fsExtra.readJson(path.resolve(actualPath, PACKAGE));
         if (pkg && pkg.exports) {
           const actualPkgPath = path.resolve(actualPath, pkg.exports);
-          return fs.existsSync(actualPkgPath) ? actualPkgPath : '';
+          if (fs.existsSync(actualPkgPath)) {
+            return {
+              name: item,
+              path: actualPkgPath,
+            };
+          }
         }
       }
-      return '';
+      return false;
     })
-    .filter(Boolean);
+    .filter(Boolean) as plugins[];
+
+  const pluginsPaths = [...installed, ...linked];
 
   const all = await Promise.all(
-    [...installed, ...linked].map((item) => import(fileUrl(item)))
+    pluginsPaths.map((item) => import(fileUrl(item.path)))
   );
 
   const allValidPlugins = all
     .map((item) => item.default)
-    .filter((item) => {
+    .filter((item, index) => {
       const validateErr = validate(
         item,
-        text.orange('plugins'),
+        `[${text.blueGray('plugins')}] ${text.orange(pluginsPaths[index].name)}`,
         predicates.command
       );
       if (validateErr) {
